a82361514b07202dcdbb3a9fdcf1345e9958fb68
Joaquim Oliveira <joaquim.tiago1999@gmail.com>
2021-11-24 11:12:50 +0000

    24-11

--- toggleFun.js
+++ toggleFun.js
@@ -1,3 +1,15 @@
+function Seed(id) {
+    this.id = id
+}
+
+function Holes(capacity) {
+    this.capacity = capacity
+}
+
+function Board(holes_capacity) {
+    this.holes_capacity = holes_capacity
+}
+
 function toggleRules(){
     const x = document.getElementById("rules"); 
     if(x.style.display == "none"){
@@ -16,21 +28,76 @@ function toggleScore(){
         x.style.display = "none";
     }
 }
-document.addEventListener("DOMContentLoaded", function( ) {
-    var slider = document.getElementById("volume");
-    var output = document.getElementById("demo");
-    output.innerHTML = slider.value;
 
-    slider.oninput = function() {
-        output.innerHTML = this.value;
+// Current board settings
+
+let current_holes = {value:""};
+let current_seeds = {value:""};
+
+
+// This function updates values in slider
+let slider_holes ; let output_holes ;
+
+let slider_seeds ; let output_seeds ;
+
+document.addEventListener("DOMContentLoaded", function( ) {
+    // holes start function
+    slider_holes = document.getElementById("slider_holes");
+    output_holes = document.getElementById("output_holes");
+    output_holes.innerHTML = slider_holes.value;
+
+    current_holes.value = slider_holes.value;
+
+    slider_holes.oninput = function() {
+        output_holes.innerHTML = this.value;
+    }
+
+    // seeds start function
+    slider_seeds = document.getElementById("slider_seeds");
+    output_seeds = document.getElementById("output_seeds");
+    output_seeds.innerHTML = slider_seeds.value;
+    
+    current_seeds.value = slider_seeds.value;
+
+    slider_seeds.oninput = function() {
+        output_seeds.innerHTML = this.value;
     }
 })
-// isto e um teste, ignorar
-function toggletest(){
+
+
+function toggleCancel(){
+
+    slider_holes.value = current_holes.value;
+    output_holes.innerHTML = current_holes.value;
+
+    slider_seeds.value = current_holes.value;
+    output_seeds.innerHTML = current_holes.value;
+    
+}
+
+function toggleApply(){
+
+    current_holes.value = slider_holes.value;
+    current_seeds.value = slider_seeds.value;
+    
+}
+
+function debbug(){
+    console.log('current_holes.value ' + current_holes.value + ' current_seeds.value ' + current_seeds.value)
+}
+
+function board(){
 
     var rangeValue = document.getElementById("test");
 
     for(let i=0;i<=10;i++){
-    rangeValue.innerHTML += '<div>'+i+'</div>';
+        rangeValue.innerHTML += '<div class="holes border">'+i+'</div>';
     }
 }
+ // var rangeValue = document.getElementById("test");
+    // rangeValue.innerHTML = '<div>'+i+'</div>' +'<div>' + slider_holes.value + '</div>';
+// console.log(slider_holes.getAttribute('default'));
+
+    // for(let i=0;i<=10;i++){
+        // rangeValue.innerHTML += '<div>'+i+'</div>';
+    // }
