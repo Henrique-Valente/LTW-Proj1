bcb9c20dadefbb2648ef4a783507174686e3ccce
Henrique <pedrohsvalente@gmail.com>
2021-12-14 18:29:22 +0000

    Finishing game back-end

--- game.js
+++ game.js
@@ -2,18 +2,35 @@ class Game {
     constructor(size, seeds){
         let board_game = new CircularLinkedList();
         for(let i=0; i<size+2; i++){
-            board_game.add_node_end(seeds);
+            if(i != size/2 && i != size+1)board_game.add_node_end(seeds);
+            else board_game.add_node_end(0);
         }
 
         this.board = board_game;
         this.size = size+2;
+        this.player = 1;
     }
 
-    print_state(){
+    print_player(){
+        if(this.player == 1) console.log("PLAYER 1:");
+        else console.log("PLAYER 2:");
+    }
+
+    print_game(){
         this.board.printList();
     }
 
+
+    check_state(cur){
+        if(cur != this.board.go_to_pos((this.size-2)/2) && cur != this.board.go_to_pos(this.size-1)){
+            this.player = 2;
+        }
+        this.print_game();
+    }
+
     move_pieces(element){
+        this.print_player();
+        this.print_game();
         let pos = element; //Terá de ser element.value quando implementarmos os botões
         let cur = this.board.go_to_pos(pos);
         let temp = cur.element;
@@ -22,7 +39,16 @@ class Game {
         cur = cur.next;
         for(let i=0; i<temp; i++){
             cur.element++;
-            cur = cur.next;
+            if(i != temp-1)cur = cur.next;
         }
+
+        this.check_state(cur);
     }
 }
+
+let test = new Game(12,4);
+
+test.print_game();
+test.move_pieces(2);
+test.move_pieces(3);
+test.move_pieces(6);
